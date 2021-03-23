322d18583b4e456815570b737c6c36845bac5aef
Jaeuk-YOO <ryou9305@naver.com>
2021-03-23 20:26:29 +0900

    add: SearchSideNav 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,12 +1,16 @@
+import * as React from "react";
+
 import Map from "../components/map/Map";
 import Sidebar from "../components/sidebar/Sidebar";
-import * as React from "react";
+
+import SearchSideNav from "../components/SearchSideNav";
 
 function Main(){
     return(
         <>
             <Sidebar/>
             <Map/>
+            <SearchSideNav />
         </>
     )
 }
